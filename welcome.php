3dd548cd83922e9e11fd1c522f6044bd4664f68c
ivy <597055914@qq.com>
2014-06-25 19:54:24 +0800

    welcome

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+</head>
+Welcome <?php echo $_POST["name"]; ?>.<br />
+You are <?php echo $_POST["age"]; ?> years old.
+<body>
+</body>
+</html>

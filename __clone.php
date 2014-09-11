90240c652b445471db047f7847037deae94aeb23
ivy <597055914@qq.com>
2014-09-11 22:41:59 +0800

    __clone

---
+++ __clone.php
@@ -0,0 +1,35 @@
+<?php
+
+class person{
+	
+	public $name;
+	public $age;
+	public $id;
+
+	function __construct($name,$age){
+		$this->name=$name;
+		$this->age=$age;
+	}
+
+	function setID($num){
+		$this->id=$num;
+	}
+
+	function __clone(){
+		$this->id=0;
+	}
+
+}
+
+$per=new person("fuck",18);
+$per->setID(20);
+
+$per2=clone $per;
+
+echo "per1.id=".$per->id.";<br>";
+
+echo "per2.age=".$per2->age.";<br>";
+echo "per2.name=".$per2->name.";<br>";
+echo "per2.id=".$per2->id.";<br>";
+
+?>

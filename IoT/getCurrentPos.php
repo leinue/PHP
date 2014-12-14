c5014a90ab328681c0717d3ad20c938c47665116
ivy <597055914@qq.com>
2014-12-14 12:04:00 +0800

    update

---
+++ IoT/getCurrentPos.php
@@ -0,0 +1,21 @@
+<?php
+
+$cls=locateByCURL($message);
+$pos='{"x":"'.$cls->content->point->x.'","y":"'.$cls->content->point->y.'"}';
+echo $pos;
+
+function locateByCURL($msg){
+	$url="http://api.map.baidu.com/location/ip";
+	$curl=curl_init($url);
+	curl_setopt($curl,CURLOPT_FAILONERROR,1);//如果发生错误直接停止运行
+	curl_setopt($curl,CURLOPT_FOLLOWLOCATION,1);//使CURL支持重定向
+	curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);//允许CURL请求返回数据
+	curl_setopt($curl,CURLOPT_TIMEOUT,5);//设置timeout为5秒
+	curl_setopt($curl,CURLOPT_POST,1);//使用POST方法
+	curl_setopt($curl,CURLOPT_POSTFIELDS,"ak=qiryZH459sCKgpGEh9EkoDx0&coor=bd09ll");//发送post数据
+	$r=curl_exec($curl);
+	curl_close($curl);
+	$r=json_decode($r);
+	return $r;
+}
+?>
--- IoT/index.php
+++ IoT/index.php
@@ -3,9 +3,9 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="css/style.css" />
 	<script>document.write(unescape('%3Cscript%20type%3D%22text/javascript%22%20src%3D%22http%3A//api.map.baidu.com/api%3Fv%3D2.0%26ak%3D52E434d922f90a508da65f332d406553%22%3E%3C/script%3E'));</script>
-	<title>酒驾监控系统Web控制端</title>
+	<title>酒驾监控系统Web控制端 V1.1</title>
 </head>
 <body>
 	<div class="content-head">
@@ -33,120 +33,6 @@
 		<div class="content-footer-flag">酒驾监控系统Web控制端·挑战杯·2014</div>
 	</div>
 
+	<script type="text/javascript" src="js/ajax.js"></script>
 </body>
-<script type="text/javascript">
-
-	//绘制百度地图
-	var map=new BMap.Map("allmap");
-
-	var longitude=document.getElementById('longitude').value;
-	var latitude=document.getElementById('latitude').value;
-
-	map.centerAndZoom(new BMap.Point(longitude,latitude),11);
-	map.enableScrollWheelZoom(true);
-
-	//使用XHR获得最新的坐标信息
-	//使用XHR轮询
-	var xhr=new XMLHttpRequest();
-
-	//天眼定位XHR
-	var locate=new XMLHttpRequest();
-
-	if(window.sessionStorage){
-	}else{
-		alert("您的浏览器不支持sessionStorage,有些功能无法使用");
-	}
-
-	getCoordinate();
-	var isfirst=1;
-
-	//window.addEventListener("load",getCoordinate,true);
-	window.setInterval(getCoordinate,2000);
-
-	//得到最新的坐标信息
-	function getCoordinate(){
-		if(typeof xhr.withCredentials===undefined){
-		
-		}else{
-			xhr.open("get","req.php?method=read",true);
-			xhr.send();
-		}
-	}
-
-	document.getElementById('submitLongAndLat').onclick=function(){
-		var longitude=document.getElementById('longitude').value;
-		var latitude=document.getElementById('latitude').value;
-		if(longitude!="" && latitude!=""){
-			toTheLocation(longitude,latitude);
-		}
-	};
-
-	document.getElementById('ajaxlocate').onclick=function(){
-		if(typeof locate.withCredentials===undefined){
-		
-		}else{
-			locate.open("get","locate.php?message=hhhhhhh",true);
-			locate.send();
-		}
-	};
-	
-	// 用经纬度设置地图中心点
-	function toTheLocation(long,lat){
-		if(long!="" && lat!=""){
-			map.clearOverlays();
-			var new_point = new BMap.Point(long,lat);
-			var marker = new BMap.Marker(new_point);  // 创建标注
-			map.addOverlay(marker);              // 将标注添加到地图中
-			map.panTo(new_point);
-		}
-	}
-
-	//locateXHR请求状态
-	locate.onreadystatechange=function(){
-		if(locate.readyState==4 && locate.status==200){
-			var res=locate.responseText;
-			var resJSON=JSON.parse(res);
-
-			displayRequestResult(resJSON.message);
-		}else{
-			displayRequestResult(xhr.statusText);
-		}
-	};
-
-	//XHR请求状态
-	xhr.onreadystatechange=function(){
-		if(xhr.readyState==4 && xhr.status==200){
-			//成功
-			var coordinate=xhr.responseText;
-			var jsondata=JSON.parse(coordinate);
-
-			var nextCount=jsondata.count;
-
-			if(isfirst==1){
-				sessionStorage['count']=sessionStorage['count']-1;
-				isfirst=0;
-			}
-			//alert("sessionStorage['count']="+sessionStorage['count']+";next="+nextCount);
-			if(nextCount!=sessionStorage['count']){
-				sessionStorage['count']=jsondata.count;
-				displayCoordinate(jsondata.longitude,jsondata.latitude);
-				toTheLocation(jsondata.longitude,jsondata.latitude);
-			}
-		}else{
-			//失败
-			displayRequestResult(xhr.statusText);
-		}
-	};
-
-	//展示最新的经纬度
-	function displayCoordinate(long,lat){
-		document.getElementById('longitude').value=long;
-		document.getElementById('latitude').value=lat;
-	}
-
-	//展示XHR状态
-	function displayRequestResult(status){
-		document.getElementById('console-status').innerHTML=status;
-	}
-</script>
 </html>

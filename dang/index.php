f3ea06f65746e597ae0dd146324cda747c96f02d
ivy <597055914@qq.com>
2015-01-03 21:32:04 +0800

    update

---
+++ dang/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	<link rel="stylesheet" href="style.css" />
+	<title>江西省高校新生党的基本知识电视竞答赛</title>
+</head>
+<body>
+	<div class="main-title">
+		<h2>江西省高校新生党的基本知识电视竞答赛</h2>
+	</div>
+
+	<div class="main-left">
+		<ul class="depot-type">
+			<li class="menuHeader">单选题</li>
+			<li class="menuHeader">多选题</li>
+		</ul>
+	</div>
+
+	<div class="main-right">
+		<div class="tabContainer"> 
+			<div class="contents">
+				<div class="tabContent">
+					<div class="tabContentTitle">
+						<span>单选题</span>
+					</div>
+					<div class="single">
+						<p>题目区域,单击随机抽题开始答题</p>
+					</div>
+				</div>
+				<div class="tabContent">
+					<div class="tabContentTitle">
+						<span>多选题</span>
+					</div>
+					<div class="multi">
+						<p>题目区域,单击随机抽题开始答题</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="main-topright">
+		<div class="random-area">随机抽题</div>
+		<div class="display-answer">显示答案</div>
+	</div>
+
+	<div class="flag">0</div>
+
+	<div class="footer">
+		Powered by <a href="http://ivydom.com" target="_blank">xieyang</a>
+	</div>
+	<script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery/jquery-1.4.min.js"></script>
+	<script type="text/javascript" src="operation.js" charset="gb2312"></script>
+</body>
+</html>

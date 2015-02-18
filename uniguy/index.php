5e090ce23491b06874b30ba2476bc174b22aebd1
ivy <597055914@qq.com>
2015-02-18 16:00:10 +0800

    update

--- uniguy/index.php
+++ uniguy/index.php
@@ -4,38 +4,44 @@
 		<meta charset="utf-8">
 		<title>UniGuy</title>
 		<link href="http://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">
-        <img alt="Brand" src="...">
-      </a>
-    </div>
+	<div class="container-fluid">
+	<div class="row">
+		<nav class="navbar navbar-inverse nav-own">
+  			<div class="container-fluid">
+    		<!-- Brand and toggle get grouped for better mobile display -->
+    			<div class="navbar-header">
+      				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        				<span class="sr-only">Toggle navigation</span>
+        				<span class="icon-bar"></span>
+        				<span class="icon-bar"></span>
+        				<span class="icon-bar"></span>
+      				</button>
+      				<a class="navbar-brand" href="#">
+        				<img alt="Brand" width="20" height="20" src="http://localhost/wordpress/wp-content/themes/uniguy/brand.png">
+      				</a>
+    			</div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="搜索 uniguy">
-        </div>
-      </form>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+    			<!-- Collect the nav links, forms, and other content for toggling -->
+    			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      				<ul class="nav navbar-nav">
+        				<li class="active"><a href="#">首页 <span class="sr-only">(current)</span></a></li>
+        				<li><a href="#">服务</a></li>
+        				<li><a href="#">案例</a></li>
+        				<li><a href="#">新闻</a></li>
+        				<li><a href="#">技术支持</a></li>
+        				<li><a href="#">联系我们</a></li>
+        				<li><a href="#"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a></li>
+      				</ul>
+    			</div><!-- /.navbar-collapse -->
+  			</div><!-- /.container-fluid -->
+		</nav>
+	</div>
+	</div>
+	<script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>
+    <script src="http://cdn.bootcss.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 	</body>
 </html>

493b7d51e84e967af71c3e7afff70ac2218a22aa
ivy <597055914@qq.com>
2015-02-18 19:18:13 +0800

    update

--- uniguy/index.php
+++ uniguy/index.php
@@ -42,16 +42,59 @@
   			</div><!-- /.container-fluid -->
 		</nav>
 		
+		<section>
+
 		<div class="carousel">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="main-title">
-						<h1>开启一份新意</h1>
+						<a href="#"><h1>开启一份新意</h1></a>
 					</div>
+				</div>
+				
+			</div>
+		</div>
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="news-display">
+					<div class="col-md-3 news-block">
+					
+					</div>
+  					<div class="col-md-3 news-block">
+  					
+  					</div>
+  					<div class="col-md-3 news-block">
+  					
+  					</div>
+  					<div class="col-md-3 news-block">
+  					
+  					</div>
 				</div>
 			</div>
 		</div>
-		
+
+		</section>
+
+		<footer>
+			<div class="container-fluid col-md-offset-1">
+				<div class="row">
+					<div class="col-md-3 col-md-offset-1">
+						<address>
+  							<strong class="footer-menu">UniGuyIT, Inc.</strong><br>
+  							<p class="text-muted footer-menu">上海 <strong><abbr title="Phone">P:</abbr> (123) 456-7890</strong></p>
+						</address>
+					</div>
+					<div class="col-md-4 col-md-offset-3">
+						<address>
+  							<p class="text-muted footer-menu"><a href="#">诸君资讯</a> | <a href="#">工作机会</a> | <a href="#">联系我们</a></p>
+  							<p class="text-muted footer-menu">京公安网安备 11010500896 京ICP备10214630</p>
+						</address>
+					</div>
+					
+				</div>
+			</div>
+		</footer>
 	</div>
 	</div>
 	<script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>

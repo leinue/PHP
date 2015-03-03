4e977f08d90acaaa2267d9ce405f7d1e6063c81a
ivy <597055914@qq.com>
2015-03-04 07:58:37 +0800

    update

--- remoteSpeaking/index.php
+++ remoteSpeaking/index.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="zh-cn">
 <head>
 	<title>My remoteSpeaking</title>
+	<meta charset="utf-8">
 </head>
 <body>
 	
+	<div data-role="page">
+
+  		<div data-role="header" data-position="fixed">
+  			<a href="#" data-role="button">首页</a>
+    		<h1>My Remote Speaking</h1>
+  		</div>
+
+  		<div data-role="content">
+    		<a href="#pagetwo" data-role="button" data-shadow="false" data-inline="true">提交</a>
+  		</div>
+
+  		<div data-role="footer" data-position="fixed">
+    		<h1>@copyright <a href="http://ivydom.com">ivydom.com</a></h1>
+  		</div>
+
+	</div>
 
 	<link rel="stylesheet" type="text/css" href="style.css" />
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">

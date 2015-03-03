007a51c89ca4c76a540fa5e491dfcb4b43f1791b
ivy <597055914@qq.com>
2015-03-04 07:48:57 +0800

    create remoteSpeaking

---
+++ remoteSpeaking/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My remoteSpeaking</title>
+</head>
+<body>
+	
+
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
+	<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+</body>
+</html>

44299e940ed8c0ea884adfae021e0c690d57c2ae
ivy <597055914@qq.com>
2015-03-04 09:02:50 +0800

    update

--- remoteSpeaking/index.php
+++ remoteSpeaking/index.php
@@ -15,7 +15,14 @@
 
   		<div data-role="content">
   			<textarea data-role class="text-input-field" placeholder="请输入远程语音文本"></textarea>
-    		<a href="#pagetwo" data-role="button" data-shadow="false" data-inline="true">提交</a>
+  			<div class="ui-grid-a">
+     			<div class="ui-block-a">
+     				<a href="#pagetwo" id="submit-remote-text" data-role="button" data-shadow="false" data-inline="true">提交</a>
+     			</div>
+     			<div class="ui-block-b">
+					<a href="#" data-role="button" id="send-status" data-shadow="false" data-icon="info">等待发送</a>
+     			</div>
+  			 </div>
   		</div>
 
   		<div data-role="footer" data-position="fixed">
@@ -28,5 +35,31 @@
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
 	<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			var remoteText=$('.text-input-field').val();
+
+			function updateStatus(s){
+				$('#send-status').html(s);
+			}
+
+			updateStatus('fuck');
+
+			$('#submit-remote-text').on('click',function(){
+				var url='send.php';
+				$.get(
+					url,
+					{c:remoteText},
+					function(data,status){
+						if(data!='-1'){
+							updateStatus('发送成功');
+						}else{
+							updateStatus('发送失败');
+						}
+					}
+				);
+			});
+		});
+	</script>
 </body>
 </html>

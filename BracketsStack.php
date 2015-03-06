5e8382e7d9dcd3ac1ab6085ac9cdfd2e0b3d40cd
ivy <597055914@qq.com>
2015-03-06 13:01:52 +0800

    update stack

---
+++ BracketsStack.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+*  Stack类
+*/
+class Stack{
+	private $top;
+	private $elem=array();
+	private $size;
+	
+	function __construct(){
+		$this->top=0;
+	}
+
+	function clear(){
+		$this->top=0;
+	}
+
+	function length(){
+		return $this->top-1;
+	}
+
+	function isEmpty(){
+		return $this->top==0;
+	}
+
+	function push($e){
+		$this->elem[$this->top++]=$e;
+		echo $e;
+		return true;
+	}
+
+	function peek(){
+		if(!($this->isEmpty())){
+			return $this->elem[$this->top-1];
+		}else{
+			return null;
+		}
+	}
+
+	function pop(){
+		if(!($this->isEmpty())){
+			$res=$this->elem[--$this->top];
+			echo 'poptop='.$this->top;
+			return $res;
+		}else{
+			return null;
+		}
+	}
+
+	function display(){
+		//echo 'length='.$this->length();
+		for($i=$this->length();$i>=0;$i--){
+			print($this->elem[$i]."  ");
+		}
+	}
+}
+
+$stack=new Stack();
+$stack->push('1');
+$stack->push('2');
+
+$stack->display();
+
+echo 'pop='.$stack->pop();
+echo 'display=';
+$stack->display();
+
+?>

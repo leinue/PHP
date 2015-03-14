f356d6d69f406cd33e358a17c42e85250916e52b
ivy <597055914@qq.com>
2015-03-14 16:09:29 +0800

    update

--- IoT/footer.php
+++ IoT/footer.php
@@ -3,12 +3,12 @@
 	</div>
 
 	<script type="text/javascript" src="js/ajax.js"></script>
-	<!--<script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>-->
+	<script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
 
       		//添加动画效果
-      		$('h3,h5 span,ul li,input').hover(
+      		$('h3,h5 span,.main-title li,.textbox').hover(
         		function(){$(this).stop().animate({opacity:0.6},400);},
         		function(){$(this).stop().animate({opacity:1},400);}
       		);
--- IoT/header.php
+++ IoT/header.php
@@ -20,13 +20,15 @@ function echoActive($title,$now){
 	<title><?php echo $title; ?></title>
 </head>
 <body>
+	<div class="big-title"><h3>“醉翁禁驾”——基于物联网的远程酒驾监控系统</h3></div>
 	<div class="content-head">
 		<ul class="main-title">
-			<li id="service-title"><a href="index.php">酒驾监控</a></li>
+			<li id="service-title"><a href="index.php"><img width="20" height="20" src="img/logo.png"></a></li>
 			<li <?php echoActive($title,"首页"); ?> ><a href="index.php">首页</a></li>
-			<li id="submitLongAndLat"><a href="">查询经纬度</a></li>
-			<li id="ajaxlocate"><a href="">天眼定位</a></li>
-			<li id="getCurrentPos"><a href="">获得当前位置</a></li>
+			<li id="getCurrentPos"><a href="">位置查询</a></li>
+			<li id="ajaxlocate"><a href="">天眼监控</a></li>
+			<li id="submitLongAndLat"><a href="">汽车防盗</a></li>
+			<li><a href="">交通服务</a></li>
 			<li <?php echoActive($title,"关于我们"); ?> id="about"><a href="about.php">关于我们</a></li>
 			<li <?php echoActive($title,"联系我们"); ?> id="contact"><a href="contact.php">联系我们</a></li>
 		</ul>

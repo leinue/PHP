686c55d87924a37b950c0b5c764690402d35446e
ivy <597055914@qq.com>
2015-03-21 21:44:48 +0800

    update

--- uniguy/uniguy2/footer.php
+++ uniguy/uniguy2/footer.php
@@ -7,9 +7,15 @@
 	<script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>
 	<script>
 		$(document).ready(function(){
+			//搜索框是否已展示
+			var inputSearchIsDiaplayed=false;
+			//小屏幕的菜单项是否已展示
+			var smallScreenMenuIsDisplayed=false;
 
+			//隐藏搜索框
 			$('.input-search').hide();
 
+			//按钮动态效果
 			$('#btn-search,.heading-menu').hover(
 				function(){
 					$(this).stop().animate({opacity:0.6},'fast');
@@ -19,24 +25,62 @@
 				}
 			);
 
-			var inputSearchIsDiaplayed=false;
+			//搜索框动态加载
 			$('#btn-search').click(function(){
-				
 				if(inputSearchIsDiaplayed){
 					$(".input-search").animate({
-    					left:'150px',
-    					width:'-=140px'
+    					left:'160px',
+    					width:'-=240px'
+  					},400,function(){
+  						$('.input-search').toggle();
   					});
   					inputSearchIsDiaplayed=false;
 				}else{
+					$('.input-search').toggle();
 					$(".input-search").animate({
-    					left:'250px',
-    					width:'+=140px'
+    					left:'160px',
+    					width:'+=240px'
   					});
   					inputSearchIsDiaplayed=true;
 				}
-				$('.input-search').toggle();
 			});
+
+			//小屏幕时菜单弹出
+			$('#list-toggle').click(function(){
+				$('.heading-bottom').slideToggle();
+				var elemHeight=$('.heading-bottom .heading-list').height()+34;
+				console.log(elemHeight);
+				if(smallScreenMenuIsDisplayed){
+					if(screen.width<=320){
+						$('.gallery-content img,.gallery-heading').animate({
+							marginTop:'-='+elemHeight+'px'
+						});
+					}
+					//$('.heading-bottom').css('borderTop','1px solid rgb(255,255,255)');
+					smallScreenMenuIsDisplayed=false;
+				}else{
+					//$('.heading-bottom').css('borderTop','none');
+					if(screen.width<=320){
+						$('.gallery-content img,.gallery-heading').animate({
+							marginTop:'+='+elemHeight+'px'
+						});
+						smallScreenMenuIsDisplayed=true;
+					}
+				}
+			});
+
+			//点击其它区域时
+			$(document).click(function(event){
+
+        		/*if(inputSearchIsDiaplayed){
+        			//$('.input-search').toggle();
+					/*$(".input-search").animate({
+    					left:'150px',
+    					width:'-=140px'
+  					});*/
+  					//inputSearchIsDiaplayed=false;
+				//}
+      		});
 
 		});
 	</script>
--- uniguy/uniguy2/header.php
+++ uniguy/uniguy2/header.php
@@ -13,16 +13,29 @@
 	<nav>
 		<div class="heading">
 			<div class="heading-list">
-				<ul>
-					<li class="heading-menu"><a href=""><img id="brand" src="http://localhost/wordpress/wp-content/themes/uniguy2/brand.png" width="20" height="20"></a></li>
-					<li class="heading-menu"><a href="">首页</a></li>
-					<li class="heading-menu"><a href="">服务</a></li>
-					<li class="heading-menu"><a href="">案例</a></li>
-					<li class="heading-menu"><a href="">支持</a></li>
-					<li class="heading-menu"><a href="">联系</a></li>
-					<li class="heading-menu"><a href="javascript:void('')"><img id="btn-search" src="http://localhost/wordpress/wp-content/themes/uniguy2/imgs/search.png" width="20" height="20"></a></li>
-					<li class="heading-menu"><input class="input-search" type="search" placeholder="搜索uniguy.com"></li>
+				<ul class="heading-big">
+					<li class="heading-menu heading-brand"><a href=""><img id="brand" src="http://localhost/wordpress/wp-content/themes/uniguy2/brand.png" width="20" height="20"></a></li>
+					<li class="heading-menu heading-o"><a href="">首页</a></li>
+					<li class="heading-menu heading-o"><a href="">服务</a></li>
+					<li class="heading-menu heading-o"><a href="">案例</a></li>
+					<li class="heading-menu heading-o"><a href="">支持</a></li>
+					<li class="heading-menu heading-o"><a href="">联系</a></li>
+					<li class="heading-menu heading-o"><a href="javascript:void('')"><img id="btn-search" src="http://localhost/wordpress/wp-content/themes/uniguy2/imgs/search.png" width="20" height="20"></a></li>
+					<li class="heading-menu heading-o"><input class="input-search" type="search" placeholder="搜索 uniguy.com"></li>
+					<li class="heading-menu heading-toggle"><img id="list-toggle" src="http://localhost/wordpress/wp-content/themes/uniguy2/imgs/toggle_list.png" width="20" height="20"></li>
 				</ul>
+			</div>
+			<div class="heading heading-bottom">
+				<div class="heading-list">
+					<ul>
+						<li class="heading-menu heading-o"><a href="">首页</a></li>
+						<li class="heading-menu heading-o"><a href="">服务</a></li>
+						<li class="heading-menu heading-o"><a href="">案例</a></li>
+						<li class="heading-menu heading-o"><a href="">支持</a></li>
+						<li class="heading-menu heading-o"><a href="">联系</a></li>
+						<li class="heading-menu heading-o"><a href="">搜索</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</nav>

d37cf5a814af6b44bcdc97b72590974b744c4303
ivy <597055914@qq.com>
2015-04-08 22:47:45 +0800

    update

--- uniguy/uniguy2/index.php
+++ uniguy/uniguy2/index.php
@@ -2,7 +2,7 @@
 
 <section>
 	<div class="gallery-content">
-		<img src="http://images.apple.com/v/home/bq/images/macbook_large.jpg">
+		<!--<img src="http://images.apple.com/v/home/bq/images/macbook_large.jpg">-->
 		<div class="gallery-heading">
 			<h1><a href=""><?php bloginfo('description'); ?></a></h1>
 		</div>
@@ -10,10 +10,10 @@
 
 	<div class="promos">
 		<ul class="news-mo">
-			<li><a href=""><img src="http://images.apple.com/v/home/bq/images/promo_world_gallery_large.jpg"></a></li>
-			<li><a href=""><img src="http://images.apple.com/v/home/bq/images/promo_blog_large.jpg"></a></li>
-			<li><a href=""><img src="http://images.apple.com/v/home/bq/images/promo_changes_ipad_large.jpg"></a></li>
-			<li><a href=""><img src="http://images.apple.com/v/home/bq/images/promo_appletv_hbonow_large.jpg"></a></li>
+			<li><a href=""><!--<img src="http://images.apple.com/v/home/bq/images/promo_world_gallery_large.jpg">--></a></li>
+			<li><a href=""><!--<img src="http://images.apple.com/v/home/bq/images/promo_blog_large.jpg">--></a></li>
+			<li><a href=""><!--<img src="http://images.apple.com/v/home/bq/images/promo_changes_ipad_large.jpg">--></a></li>
+			<li><a href=""><!--<img src="http://images.apple.com/v/home/bq/images/promo_appletv_hbonow_large.jpg">--></a></li>
 		</ul>
 	</div>
 </section>
--- uniguy/uniguy2/singlepages/case.php
+++ uniguy/uniguy2/singlepages/case.php
@@ -1,5 +1,19 @@
 	<section>
 		<div class="news-block case-block">
- 
+ 			<ul>
+ 				<li><a href="">案例</a></li>
+ 				<li>
+ 					<span>展开搜索 X</span>
+ 				</li>
+ 			</ul>
+		</div>
+		<div class="column-content">
+			<div class="section-copy">
+				<h1>Uniguy IT</h1>
+				<h2>Biger than bigger</h2>
+			</div>
+			<div class="section-img-hero">
+				sss
+			</div>
 		</div>
 	</section>

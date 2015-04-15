ba54dc8bed3df5f69455e76e59b5cec69389578a
ivy <597055914@qq.com>
2015-04-15 18:02:58 +0800

    update

--- uniguy/uniguy2/singlepages/case.php
+++ uniguy/uniguy2/singlepages/case.php
@@ -49,7 +49,7 @@
 		<div class="column-content">
 			<div class="section-copy">
 				<p class="case-main-title">Uniguy IT</p>
-				<p>Biger than bigger</p>
+				<p class="case-brief-desc">Biger than bigger</p>
 			</div>
 			<div class="section-img-hero">
 				<img src="">
@@ -61,8 +61,11 @@
 					<li><a href="">体验演讲</a></li>
 				</ul>
 			</div>
-			<div class="section-copy">
+			<div class="section-copy intro-copy">
 				<p style="transform: translate3d(0px, 26.805px, 0px);" class="intro">iPhone&nbsp;6 之大，不只是简简单单地放大，而是方方面面都大有提升。它尺寸更大，却纤薄得不可思议；性能更强，却效能非凡。光滑圆润的金属机身，与全新 Retina HD 高清显示屏精准契合，浑然一体。而软硬件间的搭配，更是默契得宛如天作之合。无论以何种尺度衡量，这一切，都让 iPhone 新一代的至大之作，成为至为出众之作。</p>
 			</div>
+		</div>
+		<div class="section-compare">
+				
 		</div>
 	</section>

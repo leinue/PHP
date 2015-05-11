20b9fc9afb0454f271ef52cb575a4ac526cdfaea
ivy <597055914@qq.com>
2015-05-11 11:29:38 +0800

    update

--- Examination/Application/Api/Controller/TagsController.class.php
+++ Examination/Application/Api/Controller/TagsController.class.php
@@ -48,30 +48,78 @@ class TagsController extends UserController {
 	}
 
 	public function update(){
-		$tag_id=I($this->requestMethod."tag-id");
+		$tag_id=I($this->requestMethod."tag_id");
 		$tag_content=I($this->requestMethod."tag_content");
-		$user_id=I($this->requestMethod."user_id");
+		//$user_id=I($this->requestMethod."user_id");
 		if(isInfoNull(array($tag_id,$tag_content,$user_id))){
+			$this->ajaxReturn(getServerResponse('0','数据不能为空',''));
+		}else{
+			/*$user_group_type=$this->getUserGroup($user_id);
+			if(!$this->isUserRootOrAdmin($user_group_type)){
+				$this->ajaxReturn(getServerResponse('0','没有权限',''));
+			}else{*/
+				$upToUser['tag_content']=$tag_content;
+				D('Tags')->where("`tag_id`=%s",array($tag_id))->data($upToUser)->save();
+				$this->ajaxReturn(getServerResponse('1','更新成功',''));
+			//}
+		}
+	}
+
+	public function get(){
+		$post_id=I($this->requestMethod."post_id");
+		$tag_id=I($this->requestMethod."tag_id");
+		if(isInfoNull(array($post_id,$tag_id))){
+			$this->ajaxReturn(getServerResponse('0','数据不能为空',''));
+		}else{
+			$rOption=M('Tags');
+			$result=$rOption->where("`post_id`=$post_id OR `tag_id`=$tag_id")->find();
+			if($result){
+				$this->ajaxReturn(getServerResponse('1','读取成功',$result));
+			}else{
+				$this->ajaxReturn(getServerResponse('0','读取失败',''));
+			}
+		}
+	}
+
+	public function remove(){
+		$tag_id=I($this->requestMethod."tag_id");
+		$user_id=I($this->requestMethod."user_id");
+		if(isInfoNull(array($tag_id,$user_id))){
 			$this->ajaxReturn(getServerResponse('0','数据不能为空',''));
 		}else{
 			$user_group_type=$this->getUserGroup($user_id);
 			if(!$this->isUserRootOrAdmin($user_group_type)){
 				$this->ajaxReturn(getServerResponse('0','没有权限',''));
 			}else{
-				$upToUser['ca_title']=$cata_title;
-				$upToUser['ca_description']=$cata_description;
-				D('Catalogue')->where("`ca_id`=%s",array($cata_id))->data($upToUser)->save();
-				$this->ajaxReturn(getServerResponse('1','更新成功',''));
+				$rOption=M('Catalogue');
+				$result=$rOption->where("`tag_id`=$tag_id")->delete();
+				if($result){
+					$this->ajaxReturn(getServerResponse('1','删除成功',$result));
+				}else{
+					$this->ajaxReturn(getServerResponse('0','删除失败',''));
+				}
 			}
 		}
 	}
 
-	public function remove(){
-
-	}
-
 	public function removeAll(){
-
+		$user_id=I($this->requestMethod."user_id");
+		if(isInfoNull(array($user_id))){
+			$this->ajaxReturn(getServerResponse('0','数据不能为空',''));
+		}else{
+			$user_group_type=$this->getUserGroup($user_id);
+			;if(!$this->isUserRootOrAdmin($user_group_type)){
+				$this->ajaxReturn(getServerResponse('0','没有权限',''));
+			}else{
+				$rOption=M('Tags');
+				$result=$rOption->where('1')->delete();
+				if($result){
+					$this->ajaxReturn(getServerResponse('1','删除成功',$result));
+				}else{
+					$this->ajaxReturn(getServerResponse('0','删除失败',''));
+				}
+			}
+		}
 	}
 
 	public function getPublicationByGuid($epid=''){

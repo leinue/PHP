a263283c79893d82ec194976c49e2c915b1b3fc9
ivy <597055914@qq.com>
2015-05-15 19:25:18 +0800

    update

--- Examination/Theme/default/Index/cg.php
+++ Examination/Theme/default/Index/cg.php
@@ -85,7 +85,7 @@
 				</div>
 				<div class="box motion-graphics">
 					<a class="iframe group1 cboxElement" href="project.html" title="">
-						<div class="box_img_div" style="background:url(images/features2.jpg) repeat center center / cover transparent!important;"></div>
+						<div class="box_img_div" style="background:url(images/hello.jpg) repeat center center / cover transparent!important;"></div>
 						<div class="mask"></div>
 						<h4>Project Title</h4>
 						<p>Some Text Here</p>
@@ -93,13 +93,293 @@
 				</div>
 			</div>
 		</div>
+
 		<div class="d_body">
-			fdfdfd
+			<div class="cg_display">
+				<div class="cg_title">
+					<ul>
+						<li style="text-align:left!important;"><h3>最新投稿</h3></li>
+						<li><a href="">更多 ></a></li>
+					</ul>
+				</div>
+				<div class="cg_content">
+					<div class="pixiv-form cg-newest">
+						<div class="box motion-graphics">
+							<a class="iframe group1 cboxElement" href="" title="">
+								<div class="box_img_div" style="background:url(images/features1.jpg) repeat center center / cover transparent!important;"></div>
+								<div class="mask"></div>
+								<h4>Project Title</h4>
+								<p>Some Text Here</p>
+							</a>
+						</div>
+						<div class="box motion-graphics">
+							<a class="iframe group1 cboxElement" href="" title="">
+								<div class="box_img_div" style="background:url(images/features.jpg) repeat center center / cover transparent!important;"></div>
+								<div class="mask"></div>
+								<h4>Project Title</h4>
+								<p>Some Text Here</p>
+							</a>
+						</div>
+						<div class="box motion-graphics">
+							<a class="iframe group1 cboxElement" href="" title="">
+								<div class="box_img_div" style="background:url(images/slogan.jpg) repeat center center / cover transparent!important;"></div>
+								<div class="mask"></div>
+								<h4>Project Title</h4>
+								<p>Some Text Here</p>
+							</a>
+						</div>
+						<div class="box motion-graphics">
+							<a class="iframe group1 cboxElement" href="" title="">
+								<div class="box_img_div" style="background:url(images/home.jpg) repeat center center / cover transparent!important;"></div>
+								<div class="mask"></div>
+								<h4>Project Title</h4>
+								<p>Some Text Here</p>
+							</a>
+						</div>
+						<div class="box motion-graphics">
+							<a class="iframe group1 cboxElement" href="" title="">
+								<div class="box_img_div" style="background:url(images/index.jpg) repeat center center / cover transparent!important;"></div>
+								<div class="mask"></div>
+								<h4>Project Title</h4>
+								<p>Some Text Here</p>
+							</a>
+						</div>
+						<div class="box motion-graphics">
+							<a class="iframe group1 cboxElement" href="" title="">
+								<div class="box_img_div" style="background:url(images/1.jpg) repeat center center / cover transparent!important;"></div>
+								<div class="mask"></div>
+								<h4>Project Title</h4>
+								<p>Some Text Here</p>
+							</a>
+						</div>
+						<div class="box motion-graphics">
+							<a class="iframe group1 cboxElement" href="" title="">
+								<div class="box_img_div" style="background:url(images/2.jpg) repeat center center / cover transparent!important;"></div>
+								<div class="mask"></div>
+								<h4>Project Title</h4>
+								<p>Some Text Here</p>
+							</a>
+						</div>
+						<div class="box motion-graphics">
+							<a class="iframe group1 cboxElement" href="" title="">
+								<div class="box_img_div" style="background:url(images/hello.jpg) repeat center center / cover transparent!important;"></div>
+								<div class="mask"></div>
+								<h4>Project Title</h4>
+								<p>Some Text Here</p>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="cg_display cg_single_theme">
+				<div class="cg_title">
+					<ul>
+						<li style="text-align:left!important;"><h3>蛤蛤</h3></li>
+						<li><a href="">更多 ></a></li>
+					</ul>
+				</div>
+				<div class="pixiv-form cg-theme">
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/features1.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/features.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/slogan.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/home.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/index.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/1.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/2.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/hello.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="cg_display cg_single_theme">
+				<div class="cg_title">
+					<ul>
+						<li style="text-align:left!important;"><h3>蛤蛤</h3></li>
+						<li><a href="">更多 ></a></li>
+					</ul>
+				</div>
+				<div class="pixiv-form cg-theme">
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/features1.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/features.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/slogan.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/home.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/index.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/1.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/2.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/hello.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="cg_display cg_top10">
+				<div class="cg_title">
+					<ul>
+						<li style="text-align:left!important;"><h3>TOP 5</h3></li>
+					</ul>
+				</div>
+				<div class="pixiv-form cg-theme">
+					<div class="box motion-graphics cg_top">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/features1.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics cg_top">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/home.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics cg_top">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/index.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics cg_top">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/hello.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+					<div class="box motion-graphics cg_top">
+						<a class="iframe group1 cboxElement" href="" title="">
+							<div class="box_img_div" style="background:url(images/1.jpg) repeat center center / cover transparent!important;"></div>
+							<div class="mask"></div>
+							<h4>Project Title</h4>
+							<p>Some Text Here</p>
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 	<script type="text/javascript">
 		$(document).ready(function(){
-			$('footer').hide();
+			$('footer').css({'margin-top':'460px'});
+
+			$('.cg_display').css('padding-top',$('.web-design').height()+$('.motion-graphics').height()-10);
+			$('.cg_single_theme').css('padding-top',$('.web-design').height()-200);
+			$('body').css('background','rgb(230,230,230)');
+			$('.cg_top10').css('top',$('.motion-graphics').height()-150);
 		});
 	</script>
 <?php  require('footer.php'); ?>

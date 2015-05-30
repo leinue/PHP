c3cb4caa8116eff29c8431dd5a9e071f9bed46c6
ivy <597055914@qq.com>
2015-05-30 23:21:28 +0800

    update

---
+++ filemanager/mysql/config.php
@@ -0,0 +1,13 @@
+<?php
+
+//配置数据库信息
+/*$dbname="test";
+$host="localhost";
+$user="root";
+$password="xieyang";
+*/
+$dbname="docmanager";
+$host="121.40.200.70";
+$user="doc";
+$password="doc";
+?>

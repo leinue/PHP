cd516d19725854c35005ff6b9f6a15ec1a937b6c
ivy <597055914@qq.com>
2015-10-01 21:34:26 +0800

    update

--- anasit/zhikangbao/src/app/examples/examples.module.js
+++ anasit/zhikangbao/src/app/examples/examples.module.js
@@ -3,20 +3,20 @@
 
     angular
         .module('app.examples', [
-            'app.examples.authentication',
-            'app.examples.calendar',
+            //'app.examples.authentication',
+            //'app.examples.calendar',
             //'app.examples.charts',
             //'app.examples.dashboards',
             'app.examples.elements',
             //'app.examples.email',
-            'app.examples.extras',
-            'app.examples.forms',
+            //'app.examples.extras',
+            //'app.examples.forms',
             //'app.examples.github',
             //'app.examples.introduction',
-            'app.examples.layouts',
-            //'app.examples.maps',
-            'app.examples.menu',
+            //'app.examples.layouts',
+            'app.examples.maps',
+            //'app.examples.menu',
             //'app.examples.todo',
-            'app.examples.ui'
+            //'app.examples.ui'
         ]);
 })();

139ed0b79b33bc95e0f22ba110aeee0467b9cd9d
ivy <597055914@qq.com>
2015-10-01 20:50:32 +0800

    update

---
+++ anasit/zhikangbao/src/app/examples/examples.module.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.examples', [
+            'app.examples.authentication',
+            'app.examples.calendar',
+            //'app.examples.charts',
+            //'app.examples.dashboards',
+            'app.examples.elements',
+            //'app.examples.email',
+            'app.examples.extras',
+            'app.examples.forms',
+            //'app.examples.github',
+            //'app.examples.introduction',
+            'app.examples.layouts',
+            //'app.examples.maps',
+            'app.examples.menu',
+            //'app.examples.todo',
+            'app.examples.ui'
+        ]);
+})();
